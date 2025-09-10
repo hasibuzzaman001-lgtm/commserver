fd0a8888d9fa1cdd66437f339d9f7a8d1cb637d1
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-09-10 10:28:19 +0600

    add domain to cors

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const corsOptions = {
     "https://www.earncorecommunity.yochrisgray.com",
     "https://www.yochrisgray.com",
     "https://yochrisgray.com",
+    "https://earncorecommunity-ge8fw6570-mdhasib01s-projects.vercel.app",
   ],
   credentials: true,
   optionsSuccessStatus: 200,
@@ -19,13 +20,8 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 };
 
-// app.use(cors(corsOptions));
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
+
 // app.options("*", cors(corsOptions));
 
 app.use(express.json({ limit: "16kb" }));
